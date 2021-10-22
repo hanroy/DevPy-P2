12889c21503bff2c47c24c8ec718c716371b03ab
HanR <h4nr0y@gmail.com>
2021-10-22 23:37:54 +0200

    add load data to csv file

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from urllib.parse import urljoin
 
 urls = ['http://books.toscrape.com/catalogue/category/books/mystery_3/index.html', 'https://books.toscrape.com/catalogue/category/books/mystery_3/page-2.html']
 
+# open a file in append mode to write into in the same directory where we ran this script from
+csvfile = open('data.csv', 'w')
+csvwriter = csv.writer(csvfile)
+fields = ["product_page_url","universal_product_code","title", "price_including_tax","price_excluding_tax","number_available","category","review_rating","image_url", "product_description"]
+csvwriter.writerow(fields)
 
 
 for url in urls:
@@ -21,12 +26,13 @@ for url in urls:
         soup = BeautifulSoup(response.content, "lxml")
 
 
-        print("Title of the book :" + soup.find('h1').get_text())
-        print("price_including_tax: " + soup.find_all('tr')[3].get_text())
-        print("price_excluding_tax: " + soup.find_all('tr')[2].get_text())     
-        print("universal_ product_code (upc): " + soup.find_all('tr')[0].get_text())
-        print("number_available: " + soup.find_all('tr')[5].get_text())
-        print("product_description :" + soup.find_all('p')[3].get_text())
-        print("category : " + soup.find_all('a')[3].get_text())
-        print("review_rating :" + soup.find_all('tr')[6].get_text())
-        print("image url :" + urljoin('https://books.toscrape.com/', soup.find("img")["src"] ))
+        universal_product_code = soup.find_all('tr')[0].get_text() 
+        title = soup.find('h1').get_text() 
+        price_including_tax = soup.find_all('td')[3].get_text()
+        price_excluding_tax = soup.find_all('td')[2].get_text()
+        number_available = soup.find_all('td')[5].get_text() 
+        product_description = soup.find_all('p')[3].get_text()
+        category = soup.find_all('a')[3].get_text() 
+        review_rating = soup.find_all('td')[6].get_text() 
+        image_url = urljoin('https://books.toscrape.com/', soup.find("img")["src"] ) 
+        csvwriter.writerow([book_url, universal_product_code.strip(), title.strip(), price_including_tax, price_excluding_tax, number_available, category, review_rating, image_url, product_description ]
