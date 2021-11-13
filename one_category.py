f143a07e1c71c565f073b9601eb9f213c9113013
HanR <h4nr0y@gmail.com>
2021-11-13 22:47:24 +0100

    raise error if url is unreachable

--- one_category.py
+++ one_category.py
@@ -4,27 +4,33 @@ from urllib.parse import urljoin #permet la gestion des url
 import csv #permet la création et manipulation des fichiers csv
 import os #permet d’effectuer des opérations courantes liées au système d’exploitation
 import re #gère les expressions régulières
+import sys #fournit des fonctions et des variables qui permettent d’interagir avec l’interpréteur Python
 
 main_url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
 
-html = requests.get(main_url)
-page = BeautifulSoup(html.content, "lxml")
-category = page.find('h1').get_text() 
+response = requests.get(main_url)
 
-urls = [main_url]
-pagination = page.select_one('li.current')
-
-if pagination is None:
-    num_pages = 1
-
+if response.status_code != 200:
+    print(f"UNREACHABLE URL response code {response.status_code}")
+    sys.exit(1)
 else:
-    page_indicator = page.find(class_='current').get_text().split()
-    num_pages = int(page_indicator[3])
+    page = BeautifulSoup(response.content, "lxml")
+    category = page.find('h1').get_text()
 
-for i in range(2, num_pages + 1):
-    parts = "page-{}.html".format(i)
-    links= urljoin(main_url, parts)
-    urls.append(links) 
+    urls = [main_url]
+    pagination = page.select_one('li.current')
+
+    if pagination is None:
+        num_pages = 1
+
+    else:
+        page_indicator = page.find(class_='current').get_text().split()
+        num_pages = int(page_indicator[3])
+
+    for i in range(2, num_pages + 1):
+        parts = "page-{}.html".format(i)
+        links= urljoin(main_url, parts)
+        urls.append(links)
 
 directory_name = './data-{}'.format(category)
 os.makedirs(directory_name, exist_ok = True)
@@ -53,16 +59,16 @@ def create_csv (urls_1):
             response = requests.get(book_url)
             soup = BeautifulSoup(response.content, "lxml")
 
-            universal_product_code = soup.find_all('tr')[0].get_text() 
-            title = soup.find('h1').get_text() 
+            universal_product_code = soup.find_all('tr')[0].get_text()
+            title = soup.find('h1').get_text()
             price_including_tax = soup.find_all('td')[3].get_text()
             price_excluding_tax = soup.find_all('td')[2].get_text()
-            number_available = soup.find_all('td')[5].get_text() 
+            number_available = soup.find_all('td')[5].get_text()
             product_description = soup.find_all('p')[3].get_text()
-            category = soup.find_all('a')[3].get_text() 
+            category = soup.find_all('a')[3].get_text()
             review_rating = soup.find('p', class_='star-rating').get('class')[1]
-            
-            image_url = urljoin('https://books.toscrape.com/', soup.find("img")["src"] ) 
+
+            image_url = urljoin('https://books.toscrape.com/', soup.find("img")["src"] )
             csvwriter.writerow([book_url, universal_product_code.strip(), title.strip(), price_including_tax, price_excluding_tax, number_available, category, review_rating, image_url, product_description ])
 
             #Download images related books
